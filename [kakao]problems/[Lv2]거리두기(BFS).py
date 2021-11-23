e45f07da47a8f28998b5320f448d2b2f85764b54
crosstar1228 <wptjd212@gmail.com>
2021-11-23 13:38:52 +0900

    2nd

--- [kakao]problems/[Lv2]거리두기(BFS).py
+++ [kakao]problems/[Lv2]거리두기(BFS).py
@@ -1,44 +1,52 @@
-from collections import deque
-def bfs(p):
-    start = []
+#https://velog.io/@sem/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-LEVEL2-%EA%B1%B0%EB%A6%AC%EB%91%90%EA%B8%B0-%ED%99%95%EC%9D%B8%ED%95%98%EA%B8%B0-Python
 
-    for i in range(5):  # 시작점이 되는 P 좌표 구하기
+from collections import deque
+
+
+def bfs(place):
+    start = []
+    for i in range(5):
         for j in range(5):
-            if p[i][j] == 'P':
+            if place[i][j] == 'P':
                 start.append([i, j])
+    # print(start)
 
     for s in start:
-        # print(s) # p점을 의미
-        queue = deque([s])
-        visited = [[0]*5 for i in range(5)] #방문처리된 곳 1로
-        distance = [[0]*5 for i in range(5)] #경로 길이
+        que = deque([s])
+        print(que)
+        visited = [[0] * 5 for i in range(5)]
+        distance = [[0] * 5 for i in range(5)]
         visited[s[0]][s[1]] = 1
-        # print(visited) # p가 지나간 점
-        while queue:
-            y, x = queue.popleft()
-            print(y,x)
-            dx = [-1, 1, 0, 0]  # 좌우
-            dy = [0, 0, -1, 1]  # 상하
 
+        dx = [1, -1, 0, 0]
+        dy = [0, 0, 1, -1]
+
+        while que:
+            y, x = que.popleft()
+            # print(y, x)
             for i in range(4):
                 nx = x + dx[i]
-                ny = y + dy[i] # nx, ny는 한 포인트 움직인 후 점 좌표
-
-                if 0<=nx<5 and 0<=ny<5 and visited[ny][nx] == 0: #인덱스 안에 있고 방문한 적이 없다
-                    queue.append([ny, nx])
+                ny = y + dy[i]
+                # print(ny,nx)
+                if 0 <= nx < 5 and 0 <= ny < 5 and visited[ny][nx] == 0 and place[ny][nx] != 'X':
+                    que.append([ny, nx])
                     visited[ny][nx] = 1
                     distance[ny][nx] = distance[y][x] + 1
+                    if place[ny][nx] == 'P' and distance[ny][nx] <= 2:
+                        return 0
 
-                if p[ny][nx] == 'P' and distance[y][x] <= 1:
-                    return 0
     return 1
 
+def solution(places):
+    answer=[]
+    for place in places:
+        answer.append(bfs(place))
+    return answer
 
 
 places = [["POOOP", "OXXOX", "OPXPX", "OOXOX", "POXXP"], ["POOPX", "OXPXP", "PXXXO", "OXXXO", "OOOPP"], ["PXOPX", "OXOXP", "OXPOX", "OXXOP", "PXPOX"], ["OOOXX", "XOOOX", "OOOXX", "OXOOX", "OOOOO"], ["PXPXP", "XPXPX", "PXPXP", "XPXPX", "PXPXP"]]
-answer=[]
-for room in places:
-    answer.append(bfs(room))
 
-print(answer)
+
+
+print(solution(places))
 
